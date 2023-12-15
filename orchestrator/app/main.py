ff65edeacf3cf8c622892ac1ca43769612fe0c81
Luis E. Fernandez de la Vara. <luis@lefv.info>
2023-12-15 06:33:24 -0500

    Added correct classification.

--- orchestrator/app/main.py
+++ orchestrator/app/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
+import spacy
+import json
 
 app = FastAPI()
+# Load spaCy model for semantic analysis
+nlp = spacy.load("en_core_web_sm")
+
+# Sample machine learning models
+def model_1(query):
+    print("Redirecting to Machine Learning Model 1 with query:", query)
+
+def model_2(query):
+    print("Redirecting to Machine Learning Model 2 with query:", query)
+
+def model_3(query):
+    print("Redirecting to Machine Learning Model 3 with query:", query)
 
 
 @app.get("/")
@@ -45,3 +59,46 @@ async def print_request(request: Request):
     print(request_dict)
 
     return {"message": "Request object printed. Check the server logs."}
+
+@app.post("/classify_query")
+@app.get("/classify_query")
+async def query(request: Request):
+    
+    body = json.loads(await request.body())
+    query = body["query"]
+    print(body["query"])
+    # return
+    # Perform semantic analysis using spaCy
+    doc = nlp(query)
+    print("received query: ", query)
+
+    # Initialize flags to check for imperative and descriptive elements
+    has_imperative = False
+    has_descriptive = False
+
+    # Iterate through tokens in the query
+    for token in doc:
+        if token.dep_ == "ROOT":
+            # Check if the root verb is an imperative verb
+            if token.tag_ == "VB" or token.tag_ == "VBG" or token.tag_ == "VBP":
+                has_imperative = True
+            else:
+                has_descriptive = True
+
+    # Determine the classification based on flags
+    if has_imperative and not has_descriptive:
+        print("Imperative Query Asked")
+        model_1(query)
+        return {"message": "Redirecting to Classification Model."}
+    elif has_descriptive and not has_imperative:
+        print("Descriptive Question Asked")
+        model_2(query)
+        return {"message": "Redirecting to Prediction Model."}
+    else:
+        print("Unable to Classify Query")
+        model_3(query)
+        return {"message": "Redirecting to Default Model."}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.host(app, host="127.0.0.1", port=8000)
